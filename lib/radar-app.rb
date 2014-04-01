dc3ffc47508ec46d1691acd7efd0622644ba5fdb
Leonardo Mendonça <leonardo.mendonca@investtools.com.br>
2014-03-31 23:05:15 -0300

    modificando nome de arquivo e colocando o start do servidor dentro da gem

--- lib/radar-app.rb
+++ lib/radar-app.rb
@@ -1,6 +1,17 @@
 require "radar/app/version"
 require 'analyzer_controller'
+require 'radar-api'
 
 module RadarApp
-  # Your code goes here...
+  class Server
+    def self.start
+      handler = RadarApp::AnalyzerController.new
+      processor = Radar::API::AnalyzerController::Processor.new(handler)
+      transport = Thrift::ServerSocket.new(5000)
+      transportFactory = Thrift::BufferedTransportFactory.new
+      server = Thrift::ThreadedServer.new(processor, transport)
+      puts "Starting server..."
+      server.serve()
+    end
+  end
 end
