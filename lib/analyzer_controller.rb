82d46ac97b63f255f7f163f326825f8af8c600c5
Leonardo Mendonça <leonardo.mendonca@investtools.com.br>
2014-04-06 19:45:05 -0300

    renomeando modulos

--- lib/analyzer_controller.rb
+++ lib/analyzer_controller.rb
@@ -1,44 +1,45 @@
-module RadarAnalyzerApp
-  class AnalyzerController
-    attr_reader :sessions
+module Radar
+  module App
+    class AnalyzerController
+      attr_reader :sessions
 
-    def initialize
-      @sessions = {}
+      def initialize
+        @sessions = {}
+      end
+
+      def self.register_analyzer(analyzer_id, analyzer_class)
+        (@@analyzers ||= {})[analyzer_id] = analyzer_class
+      end
+
+      def on_each_day(session_id, portfolio)
+        @sessions[session_id].on_each_day(portfolio)
+      end
+
+      def on_finish(session_id, portfolio)
+        @sessions[session_id].on_finish(portfolio)
+      end
+
+      def create_session(session_id, analyzer_id)
+        analyzer = @@analyzers[analyzer_id].new
+        @sessions[session_id] = analyzer
+        analyzer.config
+      end
+
+      def dump(session_id)
+        @sessions[session_id].dump
+      end
+
+      def resume(session_id, data)
+        @sessions[session_id].resume(data)
+      end
+
+      def result(session_id)
+        @sessions[session_id].result
+      end
+
+      def destroy_session(session_id)
+        @sessions.delete(session_id)
+      end
     end
-
-    def self.register_analyzer(analyzer_id, analyzer_class)
-      (@@analyzers ||= {})[analyzer_id] = analyzer_class
-    end
-
-    def on_each_day(session_id, portfolio)
-      @sessions[session_id].on_each_day(portfolio)
-    end
-
-    def on_finish(session_id, portfolio)
-      @sessions[session_id].on_finish(portfolio)
-    end
-
-    def create_session(session_id, analyzer_id)
-      analyzer = @@analyzers[analyzer_id].new
-      @sessions[session_id] = analyzer
-      analyzer.config
-    end
-
-    def dump(session_id)
-      @sessions[session_id].dump
-    end
-
-    def resume(session_id, data)
-      @sessions[session_id].resume(data)
-    end
-
-    def result(session_id)
-      @sessions[session_id].result
-    end
-
-    def destroy_session(session_id)
-      @sessions.delete(session_id)
-    end
-
   end
 end
--- lib/radar-app.rb
+++ lib/radar-app.rb
@@ -2,16 +2,18 @@ require "radar/app/version"
 require 'analyzer_controller'
 require 'radar-api'
 
-module RadarApp
-  class Server
-    def self.start
-      handler = RadarAnalyzerApp::AnalyzerController.new
-      processor = Radar::API::AnalyzerController::Processor.new(handler)
-      transport = Thrift::ServerSocket.new(5000)
-      transportFactory = Thrift::BufferedTransportFactory.new
-      server = Thrift::ThreadedServer.new(processor, transport)
-      puts "Starting server..."
-      server.serve()
+module Radar
+  module App
+    class Server
+      def self.start
+        handler = RadarAnalyzerApp::AnalyzerController.new
+        processor = Radar::API::AnalyzerController::Processor.new(handler)
+        transport = Thrift::ServerSocket.new(5000)
+        transportFactory = Thrift::BufferedTransportFactory.new
+        server = Thrift::ThreadedServer.new(processor, transport)
+        puts "Starting server..."
+        server.serve()
+      end
     end
   end
 end
