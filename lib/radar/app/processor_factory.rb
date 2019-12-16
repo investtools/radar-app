91579acd342837acf6db57be2e395c0964d9b6ff
André Aizim Kelmanson <akelmanson@gmail.com>
2019-12-16 11:53:12 -0300

    + ApplicationError

--- lib/radar/app/processor_factory.rb
+++ lib/radar/app/processor_factory.rb
@@ -1,10 +1,34 @@
 require 'thrift/exceptions'
+require 'radar-api'
 
 module Radar
   module App
     module ProcessorFactory
+
+      class AppErrorHandler
+        def initialize(target)
+          @target = target
+        end
+
+        def method_missing(m, *args, &block)
+          begin
+            @target.send(m, *args, &block)
+          rescue => e
+            if e.kind_of?(Thrift::Exception)
+              raise e
+            else
+              raise Radar::API::ApplicationError.new(message: e.message, stacktrace: e.backtrace)
+            end
+          end
+        end
+      end
+
       def self.create_processor(superclass)
         Class.new(superclass) do
+          def initialize(handler)
+            super(AppErrorHandler.new(handler))
+          end
+
           def write_error(err, oprot, name, seqid)
             super
             raise if err.type == Thrift::ApplicationException::INTERNAL_ERROR
--- spec/lib/radar/app/processor_factory_spec.rb
+++ spec/lib/radar/app/processor_factory_spec.rb
@@ -1,10 +1,30 @@
 require 'radar/app/processor_factory'
 
 describe Radar::App::ProcessorFactory do
+  let(:handler_class) do
+    Class.new do
+    end
+  end
 
   let(:parent_processor_class) do
     Class.new do
       attr_reader :write_error_called
+
+      def raise_an_error
+        raise 'an error'
+      end
+
+      def initialize(handler)
+      end
+
+      def process_an_error
+        begin
+          raise 'an error'
+        rescue => e
+          x = Thrift::ApplicationException.new(Thrift::ApplicationException::INTERNAL_ERROR, 'Internal error')
+          write_error(x, nil, nil, nil)
+        end
+      end
 
       def process_an_error
         begin
@@ -29,7 +49,7 @@ describe Radar::App::ProcessorFactory do
 
   describe '#create_processor' do
     describe 'it creates a processor that' do
-      let(:processor) { Radar::App::ProcessorFactory.create_processor(parent_processor_class).new }
+      let(:processor) { Radar::App::ProcessorFactory.create_processor(parent_processor_class).new(nil) }
       it 'raises the exception on error' do
         expect { processor.process_an_error }.to raise_error 'an error'
       end
@@ -44,6 +64,24 @@ describe Radar::App::ProcessorFactory do
       context 'when err is not INTERNAL_ERROR' do
         it 'does not raise any exception' do
           expect { processor.process_unknown_method }.not_to raise_error
+        end
+      end
+    end
+  end
+  describe 'AppErrorHandler' do
+    let(:error_handler) { Radar::App::ProcessorFactory::AppErrorHandler.new }
+    let(:target) { double() }
+    context 'then target raises a non-Thrift exception' do
+      it 'raises an ApplicationError' do
+        allow(target).to receive(:raise_an_error).and_raise 'an error'
+        error_handler = Radar::App::ProcessorFactory::AppErrorHandler.new(target)
+        expect { error_handler.raise_an_error }.to raise_error(Radar::Api::ApplicationError)
+      end
+      context 'then target raises a Thrift exception' do
+        it 'raises the original error' do
+          allow(target).to receive(:raise_an_error).and_raise Radar::Api::AuthenticationError.new
+          error_handler = Radar::App::ProcessorFactory::AppErrorHandler.new(target)
+          expect { error_handler.raise_an_error }.to raise_error(Radar::Api::AuthenticationError)
         end
       end
     end
